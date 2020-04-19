1352763dc580e8463d8122e0f6c96f23cfb6938f
zohasalman <63588457+zohasalman@users.noreply.github.com>
2020-04-20 01:46:55 +0500

    Add files via upload

---
+++ lib/VendorList.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+class VendorList {
+  String vendorname;
+  String flag;
+  String vendorrating;
+
+  VendorList ({ this.vendorname, this.flag, this.vendorrating});
+
+
+}
+
+VendorList instance = VendorList(vendorname: 'Cloudnaan', flag: 'cloudnaan.png', vendorrating: '3.4/4' );
